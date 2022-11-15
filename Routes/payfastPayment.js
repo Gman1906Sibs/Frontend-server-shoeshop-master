30951382aeaac2e756d327d25384f00461e69ade
Gman1906Sibs <77187437+Gman1906Sibs@users.noreply.github.com>
2022-11-15 20:36:23 +0200

    initial

---
+++ Routes/payfastPayment.js
@@ -0,0 +1,15 @@
+import express from "express";
+import asyncHandler from "express-async-handler";
+
+const payfastPayment = express.Router();
+
+
+
+// GET ALL PRODUCT
+paymentRoute.get(
+  "/",
+  asyncHandler(async (req, res) => {
+    console.log(res)
+  })
+);
+
---
+++ data/Products.js
@@ -0,0 +1,81 @@
+const products = [
+  {
+   
+    name: "High porosity growth oil : 250ml",
+    image: "https://drive.google.com/file/d/18-7hsr47cVHjXiv-rcqF2vgW9PM-loRo/view?usp=sharing",
+    description:
+      "A combination of oils that controls hair loss, thickens hair,helps treat dandruff,repair split ends and boosts Hair growth .This product is used daily as a moisture sealant.Ingredients: Olive oil,Castor oil and essential oils.",
+    price: 155,
+    countInStock: 3,
+    rating: 4,
+    numReviews: 4,
+  },
+  {
+
+    name: "Low porosity growth oil : 550mls",
+    image: "https://drive.google.com/file/d/1C3bFntxUn7p_8wKemVF9PzGzwj05xtJo/view?usp=sharing",
+    description:
+      "A combination of oils that controls hair loss, thickens hair,helps treat dandruff,repair split ends and boosts Hair growth .This product is used daily as a moisture sealant .Ingredients: Jojoba oil, Grapeseed oil and essential oils.",
+    price: 155,
+    countInStock: 10,
+    rating: 2,
+    numReviews: 2,
+  },
+  {
+ 
+    name: "Body scrub : 250g",
+    image: "https://drive.google.com/file/d/1PFAjcCsq-gxAoqbcQBzcBhpCJqPnvMJz/view?usp=sharing",
+    description:
+      "This product is responsible for exfoliating, removing dead cells from the outer layer of the skin and leaving the skin soft and nourished. Ingredients: Sugar,coconut oil,shea butter and essential oils",
+    price: 120,
+    countInStock: 0,
+    rating: 3.5,
+    numReviews: 3,
+  },
+  {
+  
+    name: "Bath melt : 100g",
+    image: "https://drive.google.com/file/d/1tsnwuVMjtdOEl4jmyQbtxq0PfQzuJs7D/view?usp=sharing",
+    description:
+      "A combination of butters and oils,this product is responsible for nourishing because it contains shea butter .It can be used interchangeably as a lotion bar leaving your skin feeling silky and smooth. Ingredients:Shea butter,cocoa butter,coconut oil ,essential oils",
+    price: 80,
+    countInStock: 10,
+    rating: 5,
+    numReviews: 9,
+  },
+  {
+
+    name: "Skin brightening serum : 30mls",
+    image: "https://drive.google.com/file/d/1XC16RiFm4GpUJrc8wABogmi3vQukzRpM/view?usp=sharing",
+    description:
+      "A combination of light oils,this product is responsible for clearing skin to give the skin its natural glow. Ingredients:Grapeseed oil,jojoba oil and essential oils",
+    price: 70,
+    countInStock: 7,
+    rating: 2,
+    numReviews: 2,
+  },
+  {
+
+    name: "Beard butter : 50mls",
+    image: "https://drive.google.com/file/d/1k_7HYeCFjtxy2ptz2S-l6ungQi3hXh8I/view?usp=sharing",
+    description:
+      "Description: A natural blend of butters oils that is responsible for nourishing and protects the beard from dryness and brakage",
+    price: 100,
+    countInStock: 0,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+  
+    name: "Beard Oil : 50mls",
+    image: "https://drive.google.com/file/d/1C57DTvPApd6e1T4gmSxw8JghANxYjHKp/view?usp=sharing",
+    description:
+      "A combination of oils,this product is responsible for boosting hair growth. Ingredients:Grapeseed oil,coconut oil and essential oils",
+    price: 70,
+    countInStock: 7,
+    rating: 2,
+    numReviews: 2,
+  },
+];
+
+export default products;
